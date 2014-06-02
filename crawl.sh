727f37d774c905e3d2c59719f00f5bef21b74d28
Ricardo Usbeck <ricardo.usbeck@googlemail.com>
2014-06-02 19:58:01 +0200

    initial 'crawl' script

---
+++ crawl.sh
@@ -0,0 +1,6 @@
+wget http://www.lvz-online.de/leipzig/polizeiticker/polizeiticker-leipzig/r-polizeiticker-leipzig-seite-1.html
+wget http://www.lvz-online.de/leipzig/polizeiticker/polizeiticker-leipzig/r-polizeiticker-leipzig-seite-2.html
+wget http://www.lvz-online.de/leipzig/polizeiticker/polizeiticker-leipzig/r-polizeiticker-leipzig-seite-3.html
+wget http://www.lvz-online.de/leipzig/polizeiticker/polizeiticker-leipzig/r-polizeiticker-leipzig-seite-4.html
+wget http://www.lvz-online.de/leipzig/polizeiticker/polizeiticker-leipzig/r-polizeiticker-leipzig-seite-5.html
+wget http://www.lvz-online.de/leipzig/polizeiticker/polizeiticker-leipzig/r-polizeiticker-leipzig-seite-6.html

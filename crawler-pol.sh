e1a2941cad62cd3dab32d7a76089509d73ca8145
Manuel Stößel <manuel.stoessel@gmail.com>
2014-07-14 19:49:52 +0200

    added testscript to crawl the official police press info site

---
+++ crawler-pol.sh
@@ -0,0 +1,8 @@
+wget http://polizei.sachsen.de/de/MI_2014_30367.htm
+wget http://polizei.sachsen.de/de/MI_2014_30368.htm
+wget http://polizei.sachsen.de/de/MI_2014_30369.htm
+wget http://polizei.sachsen.de/de/MI_2014_30370.htm
+wget http://polizei.sachsen.de/de/MI_2014_30371.htm
+wget http://polizei.sachsen.de/de/MI_2014_30372.htm
+wget http://polizei.sachsen.de/de/MI_2014_30373.htm
+wget http://polizei.sachsen.de/de/MI_2014_30374.htm

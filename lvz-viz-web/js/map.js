ebd6e768cf0806354b1af87660d84c5a78a4d654
paesku <paesku@gmail.com>
2014-09-22 13:39:12 +0200

    redesigned frontend  according to tw bootstraps requirements

--- lvz-viz-web/js/map.js
+++ lvz-viz-web/js/map.js
@@ -4,10 +4,19 @@ app.controller('lvzVizCtrl', function($scope, $resource, search, getx) {
     $scope.currentPage = 4;
     var map = L.map('map').setView([51.339695, 12.373075], 10);
     // // add an OpenStreetMap tile layer
+    var u_id = 'paesku.jilhmmgd';
+    //  L.tileLayer('http://a.tiles.mapbox.com/v3/' + u_id + '/{z}/{x}/{y}.png', {
+    //     attribution: ' & copy; <a href="https://www.mapbox.com/about/maps/" target="_blank">© Mapbox © OpenStreetMap</a> contributors'
+    // }).addTo(map);
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
     var markers = new L.FeatureGroup();
+    // var sidebar = L.control.sidebar('sidebar', {
+    //     position: 'right'
+    // });
+    // map.addControl(sidebar);
+    // sidebar.show();
     getx.get({
         page: 0,
         size: 30,
@@ -75,5 +84,5 @@ app.factory('getx', function($resource) {
     });
 
 
-    
+
 })

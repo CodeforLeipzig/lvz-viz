fc94e8737a2baf79ac9424642ffbcba432d940fe
Sebastian Peters <Sebastian.Peters@gmail.com>
2015-03-01 12:05:11 +0100

    update to spring-boot-1.2.2.RELEASE
    * replace three spring annotations with aggregate @SpringBootApplication
    * set exceptiontranslation to false
    * set elasticsearch.cluster-name to policeticker
    * comment application.properties

--- lvz-viz/src/main/java/de/codefor/le/LvzViz.java
+++ lvz-viz/src/main/java/de/codefor/le/LvzViz.java
@@ -1,22 +1,18 @@
 package de.codefor.le;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
 @EnableAsync
 @EnableScheduling
 @EnableSpringDataWebSupport
+@SpringBootApplication
 public class LvzViz {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(LvzViz.class, args);
     }
 

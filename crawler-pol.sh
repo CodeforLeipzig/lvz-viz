5f810da78b2b3e7fc8e898529bb5b17d823620e5
Sebastian Peters <Sebastian.Peters@gmail.com>
2016-05-06 22:50:26 +0200

    Remove obsolete crawl scripts

--- crawler-pol.sh
+++
@@ -1,8 +0,0 @@
-wget http://polizei.sachsen.de/de/MI_2014_30367.htm
-wget http://polizei.sachsen.de/de/MI_2014_30368.htm
-wget http://polizei.sachsen.de/de/MI_2014_30369.htm
-wget http://polizei.sachsen.de/de/MI_2014_30370.htm
-wget http://polizei.sachsen.de/de/MI_2014_30371.htm
-wget http://polizei.sachsen.de/de/MI_2014_30372.htm
-wget http://polizei.sachsen.de/de/MI_2014_30373.htm
-wget http://polizei.sachsen.de/de/MI_2014_30374.htm

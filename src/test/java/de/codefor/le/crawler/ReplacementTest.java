a9c6a63c501f6f362d433e3f70839905c511e322
Sebastian Peters <Sebastian.Peters@gmail.com>
2019-11-24 14:03:45 +0100

    Implement concrete test for ES article replacement and search

--- src/test/java/de/codefor/le/crawler/ReplacementTest.java
+++ src/test/java/de/codefor/le/crawler/ReplacementTest.java
@@ -1,55 +1,61 @@
 package de.codefor.le.crawler;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
 
 import org.joda.time.DateTime;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.ActiveProfiles;
 
 import de.codefor.le.model.PoliceTicker;
 import de.codefor.le.repositories.PoliceTickerRepository;
 
-@Disabled
+@ActiveProfiles({ "test" })
 @SpringBootTest
 public class ReplacementTest {
+
+    private static final ZoneId DEFAULT_ZONE = ZoneId.systemDefault();
 
     @Autowired
     private PoliceTickerRepository policeTickerRepository;
 
-    @BeforeEach
+    @AfterEach
     public void empty() {
         policeTickerRepository.deleteAll();
     }
 
-    @Test
-    public void test() {
-        PoliceTicker pt = new PoliceTicker();
-        pt.setDatePublished(Date.from(LocalDateTime.of(2014, 8, 10, 9, 6).atZone(ZoneId.systemDefault()).toInstant()));
+    @BeforeEach
+    public void init() {
+        var pt = new PoliceTicker();
+        pt.setDatePublished(Date.from(LocalDateTime.of(2014, 8, 10, 9, 6).atZone(DEFAULT_ZONE).toInstant()));
         pt.setArticle("This is the long article");
         pt.setSnippet("snippet at its best");
-
-        policeTickerRepository.index(pt);
+        assertThat(policeTickerRepository.index(pt)).isNotNull();
 
         pt = new PoliceTicker();
-        pt.setDatePublished(Date.from(LocalDateTime.of(2014, 8, 7, 9, 6).atZone(ZoneId.systemDefault()).toInstant()));
+        pt.setDatePublished(Date.from(LocalDateTime.of(2014, 8, 7, 9, 6).atZone(DEFAULT_ZONE).toInstant()));
         pt.setArticle("This is the long article");
         pt.setSnippet("snippet at its best");
+        assertThat(policeTickerRepository.index(pt)).isNotNull();
+    }
 
-        policeTickerRepository.index(pt);
+    @Test
+    public void findAll() {
+        assertThat(policeTickerRepository.findAll(PageRequest.of(0, 20))).hasSize(2);
+    }
 
-        final DateTime fromDate = new DateTime(Date.from(LocalDateTime.of(2014, 7, 2, 0, 0).atZone(ZoneId.systemDefault()).toInstant()));
-        final DateTime toDate = new DateTime(Date.from(LocalDateTime.of(2014, 8, 8, 0, 0).atZone(ZoneId.systemDefault()).toInstant()));
-        final Page<PoliceTicker> findAll = policeTickerRepository.findAll(PageRequest.of(0, 20));
-        System.out.println(findAll.getContent());
-        final Page<PoliceTicker> res = policeTickerRepository.findByDatePublishedBetween(fromDate, toDate, PageRequest.of(0, 20));
-        System.out.println(res.getContent());
-
+    @Test
+    public void findByDatePublishedBetween() {
+        final var fromDate = new DateTime(Date.from(LocalDateTime.of(2014, 7, 2, 0, 0).atZone(DEFAULT_ZONE).toInstant()));
+        final var toDate = new DateTime(Date.from(LocalDateTime.of(2014, 8, 8, 0, 0).atZone(DEFAULT_ZONE).toInstant()));
+        assertThat(policeTickerRepository.findByDatePublishedBetween(fromDate, toDate, PageRequest.of(0, 20))).hasSize(1);
     }
 }

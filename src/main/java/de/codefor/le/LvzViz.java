091fcea72aeb1caa84edcca2757ee8db7659c9f7
Sebastian Peters <Sebastian.Peters@gmail.com>
2019-12-09 21:27:15 +0100

    Remove EnableSpringDataWebSupport
    b/c spring-boot-starter-web enables it by default

--- src/main/java/de/codefor/le/LvzViz.java
+++ src/main/java/de/codefor/le/LvzViz.java
@@ -2,13 +2,11 @@ package de.codefor.le;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
 @EnableScheduling
-@EnableSpringDataWebSupport
 @SpringBootApplication
 public class LvzViz {
 

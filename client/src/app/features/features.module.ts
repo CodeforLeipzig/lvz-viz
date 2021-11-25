1c1edc0bea7f5a4a883ce93b72a9fb603c1e0ea3
inpercima <inpercima@gmail.com>
2021-11-25 09:02:36 +0100

    First implementation with leaflet map and angular-split

--- client/src/app/features/features.module.ts
+++ client/src/app/features/features.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AngularSplitModule } from 'angular-split';
+
 import { SearchComponent } from './search/search.component';
 import { MaterialModule } from '../shared/material/material.module';
 
@@ -15,6 +17,7 @@ import { MaterialModule } from '../shared/material/material.module';
   imports: [
     CommonModule,
     HttpClientModule,
+    AngularSplitModule,
     MaterialModule,
   ],
 })
--- client/src/app/features/search/search.component.ts
+++ client/src/app/features/search/search.component.ts
@@ -1,19 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+
+import { SplitComponent } from 'angular-split';
+import * as L from 'leaflet';
 
 import { SearchService } from './search.service';
-import { pipe } from 'rxjs';
-import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'lvzviz-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, AfterViewInit {
 
   displayedColumns: string[] = ['title', 'publication'];
   dataSource = new MatTableDataSource();
   expandedElement!: null;
+  private map: any;
+
+  @ViewChild("split") split!: SplitComponent;
 
   constructor(private searchService: SearchService) { }
 
@@ -21,5 +26,20 @@ export class SearchComponent implements OnInit {
     this.searchService.getx().subscribe((response: any) => {
       this.dataSource = new MatTableDataSource(response.content);
     });
+  }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+    this.split.dragProgress$.subscribe(() => {
+      this.map.invalidateSize();
+    });
+  }
+
+  private initMap(): void {
+    this.map = L.map('map').setView([51.339695, 12.373075], 11);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(this.map);
   }
 }

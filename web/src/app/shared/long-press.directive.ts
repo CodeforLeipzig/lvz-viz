bdf8b04e6dd5c72f309ba202b28b15c4e588212e
inpercima <inpercima@gmail.com>
2022-01-26 21:20:08 +0100

    Fix lint problems
    * initial maximumWarning exceeded, updated to higher values
    * "The selector should start with one of these prefixes: "lvzviz" and "should be camelCase"

--- web/src/app/shared/long-press.directive.ts
+++ web/src/app/shared/long-press.directive.ts
@@ -6,7 +6,7 @@ import { fromEvent, merge, of, Subscription, timer } from 'rxjs';
 import { filter, map, switchMap } from 'rxjs/operators';
 
 @Directive({
-  selector: '[longPress]',
+  selector: '[lvzvizLongPress]',
 })
 export class LongPressDirective implements OnDestroy {
   private eventSubscribe: Subscription;

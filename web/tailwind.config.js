27d7a63bfaa1fa267ab90710e21a03569b880d09
Marcel Jänicke <inpercima@gmail.com>
2023-02-16 22:28:14 +0100

    fix(): Update responsive design for global app
    * change and add tailwind breakpoints to pass angular-material
    * define classes in utility layer for tailwind

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    screens: {
+      'xs': { 'max': '599.98px' },
+      'sm': { 'min': '600px', 'max': '959.98px' },
+      'md': { 'min': '960px', 'max': '1279.98px' },
+      'lg': { 'min': '1280px', 'max': '1919.98px' },
+      'xl': { 'min': '1920px' },
+      'lt-md': { 'max': '959.98px' }
+    },
     extend: {},
   },
   plugins: [],

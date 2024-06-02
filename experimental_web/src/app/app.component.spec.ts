611b8a999abb30093afd7ddd05cc9f124ab86f58
Marcel Hendrich <inpercima@gmail.com>
2024-06-02 17:24:40 +0200

    chore(): update @angular/cli to 18.0.1 and its dependencies

--- experimental_web/src/app/app.component.spec.ts
+++ experimental_web/src/app/app.component.spec.ts
@@ -1,14 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { AppComponent } from './app.component';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, NoopAnimationsModule],
-    }).compileComponents();
+    imports: [NoopAnimationsModule],
+    providers: [provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()]
+}).compileComponents();
   });
 
   it('should create the app', () => {

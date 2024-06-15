3d1e4be6058e97c8f7ffe2927440ebdcb2729b8c
Marcel Hendrich <inpercima@gmail.com>
2024-06-15 09:37:23 +0200

    chore(): update app config
    Add new provider for zonejs.
    Remove unused interceptor for DI in tests.

--- experimental_web/src/app/app.config.ts
+++ experimental_web/src/app/app.config.ts
@@ -1,9 +1,7 @@
-import { OverlayModule } from '@angular/cdk/overlay';
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [importProvidersFrom(BrowserModule, OverlayModule), provideHttpClient(), provideAnimations()],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideHttpClient(), provideAnimations()],
 };
--- experimental_web/src/app/features/search/search.component.spec.ts
+++ experimental_web/src/app/features/search/search.component.spec.ts
@@ -1,9 +1,9 @@
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
+import { provideHttpClient } from '@angular/common/http';
 import { SearchComponent } from './search.component';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
@@ -11,9 +11,9 @@ describe('SearchComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-    imports: [BrowserAnimationsModule],
-    providers: [provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()]
-}).compileComponents();
+      imports: [NoopAnimationsModule],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    }).compileComponents();
   });
 
   beforeEach(() => {

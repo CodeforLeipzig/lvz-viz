e5cc30695f7f2cc99c56e116ec2473fe6e49561f
Marcel Hendrich <inpercima@gmail.com>
2025-10-04 23:43:15 +0200

    refactor(): replace deprecated animation module
    Since version 20.2 animation is deprecated and will be removed in version 23.

--- experimental_web/src/app/app.config.ts
+++ experimental_web/src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideHttpClient(), provideAnimations()],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideHttpClient()],
 };
--- experimental_web/src/app/features/search/search.component.spec.ts
+++ experimental_web/src/app/features/search/search.component.spec.ts
@@ -1,8 +1,6 @@
+import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-
-import { provideHttpClient } from '@angular/common/http';
 import { SearchComponent } from './search.component';
 
 describe('SearchComponent', () => {
@@ -11,7 +9,6 @@ describe('SearchComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [NoopAnimationsModule],
       providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
   });
